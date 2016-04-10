3c6ae098dce3cc36872c231985ef40860e75b0f1
bumblebot1 <rm14834@bristol.ac.uk>
2016-04-10 20:10:42 +0100

    tested that seek and write and read works into mid of file need more testing still

--- user/DiskTest.c
+++ user/DiskTest.c
@@ -6,14 +6,18 @@ void DiskTest() {
   creat("fail");*/
   int fd =open("test",O_RDWR);
   char y[4098];
+  char* test="BAI GYPSANE!";
   /*x = read(fd,y,4098);
   y[x] = '\0';
   printf("Finish %d\n",x );
   printf(y);*/
   write(fd,"hello",5);
-  x = lseek(fd,210000,SEEK_L);
+  x = lseek(fd,2529,SEEK_R);
   printf("%d\n",x);
-  x = read(fd,y,2);
+  x = write(fd,test,strlen(test));
+  printf("%d\n",x);
+  x = lseek(fd,strlen(test),SEEK_L);
+  x = read(fd,y,strlen(test));
   y[x] = '\0';
   printf(y);
   printf("%d\n",x);
