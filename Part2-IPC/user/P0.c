27b62a0d4ef50a074a35399baf89eaa9403d1a13
bumblebot1 <rm14834@bristol.ac.uk>
2016-04-20 22:09:04 +0100

    pipeline sort cleaner

--- Part2-IPC/user/P0.c
+++ Part2-IPC/user/P0.c
@@ -4,36 +4,11 @@ void P0() {
   int a = makeChan(0,1);//right
   int t = 0;
   void* send;
-  int k1 = 11;
-  int k2 = 24;
-  int k3 = 389;
-  int k4 = 231;
-  int k5 = 39;
-  int k6 = 1239;
-  int k7 = 75;
-  int k8 = 245;
-  int k9 = 37;
-  int k10 = 48;
-  send = &k1;
-  writeChan(a,send);
-  send = &k2;
-  writeChan(a,send);
-  send = &k3;
-  writeChan(a,send);
-  send = &k4;
-  writeChan(a,send);
-  send = &k5;
-  writeChan(a,send);
-  send = &k6;
-  writeChan(a,send);
-  send = &k7;
-  writeChan(a,send);
-  send = &k8;
-  writeChan(a,send);
-  send = &k9;
-  writeChan(a,send);
-  send = &k10;
-  writeChan(a,send);
+  int k[10] = {11,24,389,231,39,1239,75,245,37,48};
+  for(int i = 0; i<10; i++){
+    send = k+i;
+    writeChan(a,send);
+  }
   while(1){}
 }
 
